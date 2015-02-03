2681ca3b47e758a554b23853b01d8b2426cfe1a1
Damian Zientalak <damian.zientalak@gmail.com>
2015-02-03 20:38:53 +0100

    try travis env

--- travis/bin/install-apcu.sh
+++ travis/bin/install-apcu.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
  
-# this script is in a `bin/` folder
- 
-if [ "$TRAVIS_PHP_VERSION" == "5.3" || $TRAVIS_PHP_VERSION == 'hhvm' ]
+if [ "$TRAVIS_PHP_VERSION" == "5.5" ] || [ "$TRAVIS_PHP_VERSION" == "5.6" ] ; then
 then
-    exit 0
+    printf "\n"| pecl install apcu
+    echo "extension=apcu.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 fi
- 
-printf "\n"| pecl install apcu
-echo "extension=apcu.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
