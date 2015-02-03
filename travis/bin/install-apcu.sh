2dc2387a1d5263f06835bfff0d5db49d21ee1a29
Damian Zientalak <damian.zientalak@gmail.com>
2015-02-03 20:40:03 +0100

    fix script.

--- travis/bin/install-apcu.sh
+++ travis/bin/install-apcu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
  
 if [ "$TRAVIS_PHP_VERSION" == "5.5" ] || [ "$TRAVIS_PHP_VERSION" == "5.6" ] ; then
-then
     printf "\n"| pecl install apcu
     echo "extension=apcu.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 fi
